8d1ecf3f7d17f84c082340656b4278bbd26273ab
vrundanawal <nawal.vrunda@gmail.com>
2022-08-08 10:26:10 +0100

    Create the card data to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      username: 'nature',
+      imageUrl: 'assests/tree.jpeg',
+      content: 'I saw this neat tree today',
+    },
+    {
+      title: 'Snowy Mountain',
+      username: 'Mountain Lover',
+      imageUrl: 'assests/mountain.jpeg',
+      content: 'Here is a picture of a snowy mountain',
+    },
+    {
+      title: 'Mountain Biking',
+      username: 'biking12',
+      imageUrl: 'assests/biking.jpeg',
+      content: 'I did some biking today',
+    },
+  ];
 }
